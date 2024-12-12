62450b7642bf07704867c12c9f0253ea58300864
Daniel Hidalgo <jidalgo11@gmail.com>
2024-12-12 12:21:16 -0700

    removed link highlight functionality

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 const header = document.querySelector("header");
 const copyright = document.querySelector("#copyright");
 const links = document.querySelectorAll('a[href^="#"]');
-const aboutLink = document.querySelector("a[href='#aboutme']");
 
 window.addEventListener("scroll", (e) => {
 	if (window.scrollY > 5) {
 		header.classList.add("active");
 	} else {
 		header.classList.remove("active");
-	}
-	if (window.scrollY < 200) {
-		aboutLink.classList.remove("active");
 	}
 });
 
@@ -34,25 +30,6 @@ links.forEach((link) => {
 		}
 	});
 });
-
-const sections = document.querySelectorAll(".section");
-const navLinks = document.querySelectorAll("nav ul li a");
-
-const observer = new IntersectionObserver(
-	(entries) => {
-		entries.forEach((entry) => {
-			if (entry.isIntersecting) {
-				navLinks.forEach((link) => link.classList.remove("active"));
-				document
-					.querySelector(`a[href="#${entry.target.id}"]`)
-					.classList.add("active");
-			}
-		});
-	},
-	{ threshold: 1 }
-);
-
-sections.forEach((section) => observer.observe(section));
 
 const date = new Date();
 const currentYear = date.getFullYear();
